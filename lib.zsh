626e502e4220d62a6c1bf6ed0f43d705b8b171b7
Adam Heins <mail@adamheins.com>
2018-02-16 10:09:24 -0500

    Allow lib.zsh to by symlinked.

--- lib.zsh
+++ lib.zsh
@@ -1,8 +1,17 @@
 
-# This directory.
+# Get script's directory.
 here=${0:a:h}
+
+# If the script is a symlink, we want to follow the symlink back to we can
+# access the executable.
+if [ -h $0 ]; then
+  here=$(dirname $(readlink $0))
+fi
+
+# Path to executable.
 exe="$here/lib.py"
 
+# Wrap lib executable to allow for cd functionality.
 lib() {
   if [[ $1 == "cd" ]]; then
     cd $($exe where ${@:2})
